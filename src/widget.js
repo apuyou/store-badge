432b67c1074172e6fbcbdfcde9894908f21ec56c
Arthur Puyou <arthur@puyou.fr>
2019-12-06 15:33:22 +0100

    Detect browser

--- src/widget.js
+++ src/widget.js
@@ -3,22 +3,46 @@ import { h } from 'preact';
 import appStoreBadge from './appstore.svg';
 import googlePlayBadge from './googleplay.svg';
 
-function Widget({ dataName, dataAppStoreUrl, dataGooglePlayUrl }) {
+function Badge({ name, url, svg, alt, className }) {
   return (
-    <div>
-      {dataAppStoreUrl && (
-        <a href={dataAppStoreUrl} title={dataName}>
-          <img src={appStoreBadge} alt="Download on the App Store" />
-        </a>
-      )}
-
-      {dataGooglePlayUrl && (
-        <a href={dataGooglePlayUrl} title={dataName}>
-          <img src={googlePlayBadge} alt="Get it on Google Play" />
-        </a>
-      )}
-    </div>
+    <a href={url} title={name} className={className}>
+      <img src={svg} alt={alt} />
+    </a>
   );
+}
+
+function Widget({ dataName, dataAppStoreUrl, dataGooglePlayUrl }) {
+  const appStore = (
+    <Badge
+      name={dataName}
+      url={dataAppStoreUrl}
+      svg={appStoreBadge}
+      alt="Download on the App Store"
+      className="store-badge-app-store"
+    />
+  );
+
+  const googlePlay = (
+    <Badge
+      name={dataName}
+      url={dataGooglePlayUrl}
+      svg={googlePlayBadge}
+      alt="Get it on Google Play"
+      className="store-badge-google-play"
+    />
+  );
+
+  const badges = [];
+  if (navigator.userAgent.indexOf('iOS') !== -1) {
+    badges.push(appStore);
+  } else if (navigator.userAgent.indexOf('Android') !== -1) {
+    badges.push(googlePlay);
+  } else {
+    badges.push(appStore);
+    badges.push(googlePlay);
+  }
+
+  return <div>{badges.map(badge => badge)}</div>;
 }
 
 export default Widget;
